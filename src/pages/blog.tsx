aba0a4ebb1b5a4f54fb87180a544fcce405e5687
Katie Scott <reineskat@gmail.com>
2021-02-21 01:26:37 -0800

    Swap to react-router

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRouteData } from 'react-static'
-import { Link } from '@reach/router'
+import { Link } from 'react-router-dom'
 import { Post } from 'types'
 
 export default () => {
